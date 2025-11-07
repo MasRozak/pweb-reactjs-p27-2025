2b072af6888533e9b14f4274c7ad261faee78a32
dimasptrr <dimasmp259@gmail.com>
2025-11-08 00:01:29 +0700

    update

--- p27/src/utils/token.ts
+++ p27/src/utils/token.ts
@@ -1,13 +1,22 @@
-const AUTH_TOKEN_KEY = 'authToken';
 
-export const saveToken = (token: string): void => {
-  localStorage.setItem(AUTH_TOKEN_KEY, token);
+const TOKEN_KEY = 'jwt_token';
+const EMAIL_KEY = 'user_email';
+
+export const saveAuthData = (token: string, email: string): void => {
+  localStorage.setItem(TOKEN_KEY, token);
+  localStorage.setItem(EMAIL_KEY, email);
+};
+
+
+export const removeAuthData = (): void => {
+  localStorage.removeItem(TOKEN_KEY);
+  localStorage.removeItem(EMAIL_KEY);
 };
 
 export const getToken = (): string | null => {
-  return localStorage.getItem(AUTH_TOKEN_KEY);
+  return localStorage.getItem(TOKEN_KEY);
 };
 
-export const removeToken = (): void => {
-  localStorage.removeItem(AUTH_TOKEN_KEY);
+export const getEmail = (): string | null => {
+  return localStorage.getItem(EMAIL_KEY);
 };
